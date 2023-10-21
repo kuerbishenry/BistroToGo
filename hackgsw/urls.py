3aaba9843700afb11bf2e57eb58d36457f0aea9a
ColinRuiz <colin.ruiz21@gmail.com>
2023-10-21 10:46:51 -0400

    Login feature

--- hackgsw/urls.py
+++ hackgsw/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path("", include("hack.urls")),
+    path('', include('hack.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("admin/", admin.site.urls),
 ]
