4ed55f1c23590f5455aa632e90787d075d811bf9
ColinRuiz <colin.ruiz21@gmail.com>
2023-10-21 11:57:41 -0400

    sign up page for new users. all the redirects work

--- hack/views.py
+++ hack/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,6 +14,16 @@ def index(request):
     return render(request, 'index.html')
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return render(request, 'success.html')  # Replace 'home' with your desired URL name
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def login_view(request):
@@ -24,7 +36,10 @@ def login_view(request):
             if user:
                 login(request, user)
                 # Redirect to a success page or the home page
-                return redirect('home')
+                return render(request, 'index.html')
+            else:
+                # User does not exist, provide a sign-up option
+                return render(request, 'signup.html')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
