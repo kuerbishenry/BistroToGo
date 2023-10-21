a569ec232d343f0c078c49b7d50567c43886ee6e
dillonr11 <dillonrem@yahoo.com>
2023-10-21 12:24:06 -0400

    ordering system

--- hackgsw/urls.py
+++ hackgsw/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("admin/", admin.site.urls),
+    path("order/", include("order.urls"))
 ]
