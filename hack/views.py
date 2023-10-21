6a62e2f9f124116ce2a6ef9b9aa3d84f503e0ae6
ColinRuiz <colin.ruiz21@gmail.com>
2023-10-21 17:58:41 -0400

    tesrt

--- hack/views.py
+++ hack/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, redirect
 from .Forms import *
 
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -20,8 +22,12 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log in the user after registration
-            return render(request, 'orderScreen.html')  # Replace 'home' with your desired URL name
+            login(request, user)# Log in the user after registration
+            if (user_has_completed_form(user)):
+                return render(request, 'orderScreen.html')
+                # Redirect to a success page or the home page
+            else:
+                return render(request, 'path_to_template.html')  # Replace 'home' with your desired URL name
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
@@ -36,14 +42,18 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
+                if (user_has_completed_form(user)):
+                    return render(request, 'orderScreen.html')
                 # Redirect to a success page or the home page
-                return render(request, 'orderScreen.html')
+                else:
+                    return render(request, 'path_to_template.html')
             else:
                 # User does not exist, provide a sign-up option
                 return render(request, 'signup.html')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
 
 
 def profile_update(request):
@@ -58,8 +68,25 @@ def profile_update(request):
             profile = form.save(commit=False)
             profile.user = request.user
             profile.save()
-            return render(request, 'orderScreen.html') # redirect to a success page or something similar
+            return HttpResponseRedirect(reverse('profile_update'))
     else:
         form = UserProfileForm(instance=instance)
 
     return render(request, 'path_to_template.html', {'form': form})
+
+
+def user_has_completed_form(user):
+    """
+    Check if the user has completed the user profile form.
+    Returns True if the form is complete, False otherwise.
+    """
+    try:
+        profile = UserProfile.objects.get(user=user)
+        # Check if the required fields are not empty
+        if profile.first_name and profile.last_name and profile.meal_plan and profile.current_dining_dollars:
+            return True
+    except UserProfile.DoesNotExist:
+        pass
+
+    return False
+
